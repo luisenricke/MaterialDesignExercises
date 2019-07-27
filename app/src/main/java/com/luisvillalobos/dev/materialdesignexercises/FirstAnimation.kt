3c3683ce391bec80aa3d1851a36b6b1a21eaeaa0
luisenricke <luisevm24@outlook.com>
2019-07-27 02:53:49 -0500

    feat: add animation of fade in the view

--- app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/FirstAnimation.kt
+++ app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/FirstAnimation.kt
@@ -1,7 +1,9 @@
 package com.luisvillalobos.dev.materialdesignexercises
 
+import android.opengl.Visibility
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_first_animation.*
@@ -12,12 +14,19 @@ class FirstAnimation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_animation)
 
+
+        image.visibility = View.INVISIBLE // To apply fade_in
+
         startAnimation.setOnClickListener {
             //Make bigger the view
             //var animation = AnimationUtils.loadAnimation(this, R.anim.big)
 
             //Make invisible the view
-            var animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+            //var animation = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+
+            //Make visible the view
+            var animation = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+
 //            animation.fillAfter = true // Still the size after animate
 
 //            animation.repeatMode = Animation.INFINITE
