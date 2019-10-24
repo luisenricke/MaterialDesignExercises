f95615f3b78f3749e8d28597a38226d658a263bf
luisenricke <luisevm24@outlook.com>
2019-10-24 12:33:54 -0500

    build: add new activity to test ConstraitLayout

---
+++ app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/ActivityConstraintLayout.kt
@@ -0,0 +1,12 @@
+package com.luisvillalobos.dev.materialdesignexercises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityConstraintLayout : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_layout)
+    }
+}
