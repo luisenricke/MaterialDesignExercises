04da31c1289db3c1c0f5d3f313fce60721b799f7
luisenricke <luisevm24@outlook.com>
2019-11-11 19:54:30 -0600

    build: config experimental plugin

---
+++ app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/design/MenuActivity.kt
@@ -0,0 +1,33 @@
+package com.luisvillalobos.dev.materialdesignexercises
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
+import timber.log.Timber
+
+class MenuActivity : AppCompatActivity(), MenuFragment.OnFragmentInteractionListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+
+        button.setOnClickListener {
+            var list : ArrayList<Option> = arrayListOf(
+                Option("Take picture", R.drawable.ic_android_black_24dp),
+                Option("Select from gallery",R.drawable.ic_android_black_24dp)
+            )
+            val menuFragment = MenuFragment.newInstance(list)
+            menuFragment.show(supportFragmentManager, "Menu")
+        }
+    }
+
+    override fun onItemClickRecyclerView(item: Option) {
+        when (item.name) {
+            "Take picture" -> {
+                Timber.i("Take picture")
+            }
+            "Select from gallery" -> {
+                Timber.i("Select from gallery")
+            }
+        }
+    }
+}
--- app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/design/MenuFragment.kt
+++ app/src/main/java/com/luisvillalobos/dev/materialdesignexercises/design/MenuFragment.kt
@@ -1,18 +1,28 @@
 package com.luisvillalobos.dev.materialdesignexercises
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.luisvillalobos.dev.materialdesignexercises.utils.RoundedBottomSheetDialogFragment
 
+private const val PARAM_LIST = "param_list"
 
 class MenuFragment : RoundedBottomSheetDialogFragment() {
     private var listener: OnFragmentInteractionListener? = null
+    private var listParam: ArrayList<Option>? = null
+
+    private lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            listParam = it.getParcelableArrayList(PARAM_LIST)
+        }
     }
 
     override fun onCreateView(
@@ -20,11 +30,17 @@ class MenuFragment : RoundedBottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_menu, container, false)
 
-    fun onButtonPressed() {
-        listener?.onFragmentInteraction()
+        recyclerView = view.findViewById(R.id.recyclerViewOptions)
+        recyclerView.layoutManager = GridLayoutManager(activity, 1)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = listParam?.let {
+            OptionAdapter(it) { item ->
+                listener?.onItemClickRecyclerView(item)
+            }
+        }
+        return view
     }
 
     override fun onAttach(context: Context) {
@@ -42,11 +58,16 @@ class MenuFragment : RoundedBottomSheetDialogFragment() {
     }
 
     interface OnFragmentInteractionListener {
-        fun onFragmentInteraction()
+        fun onItemClickRecyclerView(item: Option)
     }
 
     companion object {
         @JvmStatic
-        fun newInstance() = MenuFragment()
+        fun newInstance(list: ArrayList<Option>) =
+            MenuFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelableArrayList(PARAM_LIST, list)
+                }
+            }
     }
 }
